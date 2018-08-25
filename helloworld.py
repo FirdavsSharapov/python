39644730d8f33db21ea730b89f8d957a6eb94b37
Fred Sharapov <f.sharapov@hotmail.com>
2018-08-25 11:46:52 -0700

    Commit with some code in it

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,12 @@
-print("Hello  World!")
+# Importing math library
+
+
+# from math import *
+# print(sqrt(81))
+###########
+# color = input("Enter a color: ")
+# plural_noun = input("Enter a Plular noun: ")
+# celebraty =input("Enter Celebraty name: ")
+# print("Roses are" + color)
+# print(plurlar_noun + "are blue")
+# print("I love " + celebraty)
