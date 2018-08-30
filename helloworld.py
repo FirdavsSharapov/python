2727279ef149971639a042e7266dbb8324bb5d98
Fred Sharapov <f.sharapov@hotmail.com>
2018-08-29 22:53:29 -0700

    raise_to_power idea

--- helloworld.py
+++ helloworld.py
@@ -90,10 +90,19 @@
 #     print("You win!")
 
 #### FOR LOOP #############
-friends = ["Jim", "Karen", "Kevin"]
-len(friends)
-for index in range(len(friends)):
-    print(friends[index])
-    friends["Tim"]
+# friends = ["Jim", "Karen", "Kevin"]
+# len(friends)
+# for index in range(len(friends)):
+#     print(friends[index])
+#     friends["Tim"]
+#
+# print(friends)
 
-print(friends)
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+
+
+print(raise_to_power(3, 2))
