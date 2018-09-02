24e50de624a73e8e1f7af9652f8965c0cfa332b5
Fred Sharapov <f.sharapov@hotmail.com>
2018-09-01 23:27:52 -0700

    try: except err catcher

--- helloworld.py
+++ helloworld.py
@@ -98,11 +98,31 @@
 #
 # print(friends)
 
-def raise_to_power(base_num, pow_num):
-    result = 1
-    for index in range(pow_num):
-        result = result * base_num
-    return result
+# def raise_to_power(base_num, pow_num):
+#     result = 1
+#     for index in range(pow_num):
+#         result = result * base_num
+#     return result
+#
+#
+# print(raise_to_power(3, 2))
+
+###########2D dimenssional list and nested list#############
+
+# number_grid = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9],
+#     [0]
+# ]
+#
+# for row in number_grid:
+#     for col in row:
+#         print(col)
 
 
-print(raise_to_power(3, 2))
+try:
+    number = int(input("Enter a number:"))
+    print(number)
+except ValueError as err:
+    print(err)
