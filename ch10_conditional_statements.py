5d29fe96a9093bf7d2baf544639b210805648eae
Firdavs Sharapov <firdavssharapov@fb.com>
2019-02-28 11:02:00 -0800

    change on XML file

---
+++ ch10_conditional_statements.py
@@ -0,0 +1,7 @@
+customer_29876 = {'First Name' : 'Fred', 'Last Name' : 'Sharapov'}
+jobs_to_do_list = ['email','texting', 'calls']
+
+print(customer_29876["First Name"])
+
+del customer_29876["Last Name"]
+print(customer_29876["First Name"])
