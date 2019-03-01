d68b002759401903cddf2cfbb25150032e84ac02
Firdavs Sharapov <firdavssharapov@fb.com>
2019-02-28 16:14:42 -0800

    List of dictionary was added

--- ch10_conditional_statements.py
+++ ch10_conditional_statements.py
@@ -1,7 +1,30 @@
-customer_29876 = {'First Name' : 'Fred', 'Last Name' : 'Sharapov'}
-jobs_to_do_list = ['email','texting', 'calls']
+customer_29876 = {'First Name': 'Fred', 'Last Name': 'Sharapov', 'Address': '505 Elmwood Ave'}
+jobs_to_do_list = ['email', 'texting', 'calls']
 
 print(customer_29876["First Name"])
 
-del customer_29876["Last Name"]
-print(customer_29876["First Name"])
+# del customer_29876["Last Name"]
+# print(customer_29876["First Name"])
+
+for each_value in customer_29876.keys():
+    print(each_value)
+
+for e, each_value in customer_29876.items():
+    print("The customer's " + e + " is " + each_value)
+
+customers = [
+    {"Customer ID": 0,
+     "First Name": "Fred",
+     "Last Name": "Sharapov"},
+    {"Customer ID": 1,
+     "First Name": "David",
+     "Last Name": "de Heer"},
+    {"Customer ID": 2,
+     "First Name": "Faha",
+     "Last Name": "Sharapov"},
+]
+
+customer_first_name = customers[0]
+customer_name = customer_first_name["Last Name"]
+len_customer = len(customers)
+print(len_customer)
